775a8010b830fbad0dfd870fa7025102f9fe141a
Qias Me <TajaeJohnson@proton.me>
2023-05-29 18:48:46 -0500

    Version Control

--- src/Components/About/data.js
+++ src/Components/About/data.js
@@ -20,20 +20,20 @@ export const featureSectionData = [
 export const testimonialSectionData = [
   {
   id: 1,
-  name: "Mitchael Bailey - Small Business Owner",
+  name: "Mitchael - Small Business Owner",
   quote: "The team at this company made creating a website for my small business so easy and seamless. They understood my business objectives and target audience, and developed a website that perfectly reflects my brand and meets my business goals. I couldn't be happier with the results!",
   image: "https://via.placeholder.com/300",
   },
   {
   id: 2,
-  name: "Allen Beene - Student",
+  name: "Allen - Student",
   quote: "Thanks to the expert tutoring services provided by this company, I was able to catch up and even surpass my classmates in computer science. The tutor was patient and knowledgeable, and really helped me understand the material in a way that made sense to me. I would recommend them to any student struggling with STEM subjects.",
   image: "https://via.placeholder.com/300",
 
 },
   {
   id: 3,
-  name: "Rachel Davis - Parent",
+  name: "Rachel - Parent",
   quote: "My child has not only improved their grades in java programming, but also gained a new sense of confidence and enthusiasm for learning, thanks to the personalized tutoring services offered by this company. The tutor was patient, knowledgeable, and really took the time to understand my child's learning style and needs. I would highly recommend this company to any parent looking for academic support for their child.",
   image: "https://via.placeholder.com/300",
 
